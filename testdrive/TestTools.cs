f112d7fa26b8f0380626f93ade9bfdf060516ca0
Samir ahmed <samirahmed2013@gmail.com>
2013-01-24 15:41:25 -0500

    Testing Algorithmix Namespae

--- testdrive/TestTools.cs
+++ testdrive/TestTools.cs
@@ -10,120 +10,128 @@ using System.Runtime.Serialization;
 
 #endregion
 
-namespace TestTools
+namespace Algorithmix
 {
-    public class TestDriveException : Exception
+    namespace TestTools
     {
-        public TestDriveException() : base()
+        public class TestDriveException : Exception
         {
-        }
-
-        public TestDriveException(string message) : base(message)
-        {
-        }
-
-        public TestDriveException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        protected TestDriveException(SerializationInfo info,
-                                     StreamingContext context)
-        {
-        }
-    }
-
-    public class Drive
-    {
-        #region Reason enum
-
-        public enum Reason
-        {
-            Read,
-            Save
-        }
-
-        #endregion
-
-        private static string TESTDRIVE_ROOT;
-
-        private readonly string _directory;
-        private readonly Reason _reason;
-
-        public Drive(string relativePath, Reason reason)
-        {
-            var directory = Path.Combine(GetDriveRoot(), relativePath);
-            if (!Directory.Exists(directory) && reason == Reason.Read)
+            public TestDriveException()
+                : base()
             {
-                throw new TestDriveException("no such directory: " + directory);
             }
-            else if (Directory.Exists(directory) && reason == Reason.Save)
+
+            public TestDriveException(string message)
+                : base(message)
             {
-                throw new TestDriveException("Attempting to open an existing directory: " + directory);
             }
-            else if (!Directory.Exists(directory) && reason == Reason.Save)
+
+            public TestDriveException(string message, Exception inner)
+                : base(message, inner)
             {
-                Directory.CreateDirectory(directory);
             }
-            _reason = reason;
-            _directory = directory;
+
+            protected TestDriveException(SerializationInfo info,
+                                         StreamingContext context)
+            {
+            }
         }
 
-        public static string GetDriveRoot()
+        public class Drive
         {
-            if (TESTDRIVE_ROOT == null)
+            #region Reason enum
+
+            public enum Reason
             {
-                TESTDRIVE_ROOT = Environment.GetEnvironmentVariable("TESTDRIVE_ROOT");
+                Read,
+                Save
+            }
+
+            #endregion
+
+            private static string TESTDRIVE_ROOT;
+
+            private readonly string _directory;
+            private readonly Reason _reason;
+
+            public Drive(string relativePath, Reason reason)
+            {
+                var directory = Path.Combine(GetDriveRoot(), relativePath);
+                if (!Directory.Exists(directory) && reason == Reason.Read)
+                {
+                    throw new TestDriveException("no such directory: " + directory);
+                }
+                else if (Directory.Exists(directory) && reason == Reason.Save)
+                {
+                    throw new TestDriveException("Attempting to open an existing directory: " + directory);
+                }
+                else if (!Directory.Exists(directory) && reason == Reason.Save)
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                _reason = reason;
+                _directory = directory;
+            }
+
+            public static string GetDriveRoot()
+            {
                 if (TESTDRIVE_ROOT == null)
                 {
-                    throw new TestDriveException(
-                        "TESTDRIVE_ROOT Environment Variable not found - please ensure you have it set");
+                    TESTDRIVE_ROOT = Environment.GetEnvironmentVariable("TESTDRIVE_ROOT");
+                    if (TESTDRIVE_ROOT == null)
+                    {
+                        throw new TestDriveException(
+                            "TESTDRIVE_ROOT Environment Variable not found - please ensure you have it set");
+                    }
+                }
+                return TESTDRIVE_ROOT;
+            }
+
+            public IEnumerable<String> FullFilePaths()
+            {
+                return Files().Select(file => Path.Combine(_directory, file));
+            }
+
+            public IEnumerable<String> Files()
+            {
+                return Directory.EnumerateFiles(_directory);
+            }
+
+            public IEnumerable<String> Files(string prefix)
+            {
+                return
+                    Directory.EnumerateFiles(_directory).Where(
+                        filename => filename.StartsWith(Path.Combine(this._directory, prefix)));
+            }
+
+            public int FileCount()
+            {
+                return Files().Count();
+            }
+
+            public int FileCount(string prefix)
+            {
+                return Files(prefix).Count();
+            }
+
+            public void Save(Bitmap bitmap, string prefix)
+            {
+                if (_reason == Reason.Save)
+                {
+                    bitmap.Save(Path.Combine(_directory, prefix + "_" + (FileCount(prefix) + 1)), ImageFormat.Jpeg);
+                }
+                else
+                {
+                    throw new TestDriveException("Attempted to Save File in Read Only Directory");
                 }
             }
-            return TESTDRIVE_ROOT;
-        }
 
-        public IEnumerable<String> FullFilePaths()
-        {
-            return Files().Select(file => Path.Combine(_directory, file));
-        }
-
-        public IEnumerable<String> Files()
-        {
-            return Directory.EnumerateFiles(_directory);
-        }
-
-        public IEnumerable<String> Files(string prefix)
-        {
-            return Directory.EnumerateFiles(_directory).Where(filename => filename.StartsWith(Path.Combine(this._directory,prefix)));
-        }
-
-        public int FileCount()
-        {
-            return Files().Count();
-        }
-
-        public int FileCount(string prefix)
-        {
-            return Files(prefix).Count();
-        }
-
-        public void Save(Bitmap bitmap, string prefix)
-        {
-            if (_reason == Reason.Save)
+            public void Save(IEnumerable<Bitmap> bitmaps, string prefix)
             {
-                bitmap.Save(Path.Combine(_directory, prefix + "_" + (FileCount(prefix) + 1)), ImageFormat.Jpeg);
-            }
-            else
-            {
-                throw new TestDriveException("Attempted to Save File in Read Only Directory");
-            }
-        }
-
-        public void Save(IEnumerable<Bitmap> bitmaps, string prefix)
-        {
-            foreach (Bitmap bitmap in bitmaps)
-            {
-                Save(bitmap, prefix);
+                foreach (Bitmap bitmap in bitmaps)
+                {
+                    Save(bitmap, prefix);
+                }
             }
         }
     }
