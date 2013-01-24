6c5ad4ced4c12e79cf3ec1f9d266b0c84bb8baa7
Samir ahmed <samirahmed2013@gmail.com>
2013-01-24 12:36:40 -0500

    Added Functionality Test

---
+++ UnitTest/tests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.IO;
+using TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        public readonly string UnitTestFolder = "TestDriveUnitTest";
+        public readonly string TestMaterialFolder = "RawShreds";
+
+        [TestMethod]
+        public void GetDriveRootTest()
+        {
+            Assert.IsNotNull(Drive.GetDriveRoot());
+        }
+
+        [TestMethod]
+        public void TestSaveAndLoad()
+        {
+            const string saveDirectory = "TestSaving";
+            RemoveDirectory(saveDirectory);
+            var saveFolder = new TestTools.Drive(Path.Combine(UnitTestFolder, saveDirectory), TestTools.Drive.Reason.Save);
+            Assert.IsTrue( saveFolder.FileCount()==0 );
+
+            var loadFolder = new TestTools.Drive(Path.Combine(UnitTestFolder, TestMaterialFolder), TestTools.Drive.Reason.Read);
+            var totalFileCount = loadFolder.FileCount();
+            Assert.IsTrue( totalFileCount>0);
+
+            var bitmaps = new List<System.Drawing.Bitmap>(totalFileCount);
+            foreach( string filepath in loadFolder.FullFilePaths() )
+            {
+                var bitmap = new System.Drawing.Bitmap(filepath);
+                bitmaps.Add(bitmap);
+            }
+            Assert.IsTrue( bitmaps.Count==totalFileCount);
+
+            saveFolder.Save(bitmaps,"unittest");
+            Assert.IsTrue( saveFolder.FileCount("unittest")==totalFileCount);
+            Assert.IsTrue(saveFolder.FileCount()==totalFileCount);
+        }
+
+        [TestMethod]
+        public void TestExceptions()
+        {
+
+        }
+
+        private void RemoveDirectory(string directory)
+        {
+            var path = Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, directory);
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+            var files = Directory.EnumerateFiles(path);
+            foreach(string file in files)
+            {
+                File.Delete( file );
+            }
+            Directory.Delete(path);
+        }
+    }
+}
---
+++ testdrive/TestTools.cs
@@ -0,0 +1,130 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+
+#endregion
+
+namespace TestTools
+{
+    public class TestDriveException : Exception
+    {
+        public TestDriveException() : base()
+        {
+        }
+
+        public TestDriveException(string message) : base(message)
+        {
+        }
+
+        public TestDriveException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected TestDriveException(SerializationInfo info,
+                                     StreamingContext context)
+        {
+        }
+    }
+
+    public class Drive
+    {
+        #region Reason enum
+
+        public enum Reason
+        {
+            Read,
+            Save
+        }
+
+        #endregion
+
+        private static string TESTDRIVE_ROOT;
+
+        private readonly string _directory;
+        private readonly Reason _reason;
+
+        public Drive(string relativePath, Reason reason)
+        {
+            var directory = Path.Combine(GetDriveRoot(), relativePath);
+            if (!Directory.Exists(directory) && reason == Reason.Read)
+            {
+                throw new TestDriveException("no such directory: " + directory);
+            }
+            else if (Directory.Exists(directory) && reason == Reason.Save)
+            {
+                throw new TestDriveException("Attempting to open an existing directory: " + directory);
+            }
+            else if (!Directory.Exists(directory) && reason == Reason.Save)
+            {
+                Directory.CreateDirectory(directory);
+            }
+            _reason = reason;
+            _directory = directory;
+        }
+
+        public static string GetDriveRoot()
+        {
+            if (TESTDRIVE_ROOT == null)
+            {
+                TESTDRIVE_ROOT = Environment.GetEnvironmentVariable("TESTDRIVE_ROOT");
+                if (TESTDRIVE_ROOT == null)
+                {
+                    throw new TestDriveException(
+                        "TESTDRIVE_ROOT Environment Variable not found - please ensure you have it set");
+                }
+            }
+            return TESTDRIVE_ROOT;
+        }
+
+        public IEnumerable<String> FullFilePaths()
+        {
+            return Files().Select(file => Path.Combine(_directory, file));
+        }
+
+        public IEnumerable<String> Files()
+        {
+            return Directory.EnumerateFiles(_directory);
+        }
+
+        public IEnumerable<String> Files(string prefix)
+        {
+            return Directory.EnumerateFiles(_directory).Where(filename => filename.StartsWith(Path.Combine(this._directory,prefix)));
+        }
+
+        public int FileCount()
+        {
+            return Files().Count();
+        }
+
+        public int FileCount(string prefix)
+        {
+            return Files(prefix).Count();
+        }
+
+        public void Save(Bitmap bitmap, string prefix)
+        {
+            if (_reason == Reason.Save)
+            {
+                bitmap.Save(Path.Combine(_directory, prefix + "_" + (FileCount(prefix) + 1)), ImageFormat.Jpeg);
+            }
+            else
+            {
+                throw new TestDriveException("Attempted to Save File in Read Only Directory");
+            }
+        }
+
+        public void Save(IEnumerable<Bitmap> bitmaps, string prefix)
+        {
+            foreach (Bitmap bitmap in bitmaps)
+            {
+                Save(bitmap, prefix);
+            }
+        }
+    }
+}
