15cdbb2d473a5df5e4979c7f64f4a5dd654d6987
Richard Tia <richie.tia@gmail.com>
2013-02-02 19:01:21 -0500

    Added Recursive Get All Matching
    Added Test to verify that it works
    There is an explicity MAX_DEPTH that can be set
    Converted the Remove Directory to be recursive aswell

--- UnitTest/tests.cs
+++ UnitTest/tests.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Drawing;
 using System.Collections.Generic;
 using System.IO;
-using TestTools;
+using Algorithmix.TestTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
@@ -23,11 +24,11 @@ namespace UnitTest
         public void TestSaveAndLoad()
         {
             const string saveDirectory = "TestSaving";
-            RemoveDirectory(saveDirectory);
-            var saveFolder = new TestTools.Drive(Path.Combine(UnitTestFolder, saveDirectory), TestTools.Drive.Reason.Save);
+            RemoveDirectory(Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, saveDirectory));
+            var saveFolder = new Drive(Path.Combine(UnitTestFolder, saveDirectory), Drive.Reason.Save);
             Assert.IsTrue( saveFolder.FileCount()==0 );
 
-            var loadFolder = new TestTools.Drive(Path.Combine(UnitTestFolder, TestMaterialFolder), TestTools.Drive.Reason.Read);
+            var loadFolder = new Drive(Path.Combine(UnitTestFolder, TestMaterialFolder), Drive.Reason.Read);
             var totalFileCount = loadFolder.FileCount();
             Assert.IsTrue( totalFileCount>0);
 
@@ -50,9 +51,45 @@ namespace UnitTest
 
         }
 
-        private void RemoveDirectory(string directory)
+        [TestMethod]
+        public void TestGetAllMatching()
         {
-            var path = Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, directory);
+            const string dir = "TestRecursiveMatching";
+            RemoveDirectory(Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, dir));
+            var expected = new List<string>();
+            BuildNestedDirectory(Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, dir),expected);
+            var drive = new Drive(Path.Combine(UnitTestFolder, dir), Drive.Reason.Read);
+            var actual  =drive.GetAllMatching("image");
+            expected.Sort();
+            actual.Sort();
+            var same = expected.Zip(actual,(first,second) => first==second).All(x => x==true);
+            Assert.IsTrue(same);
+        }
+
+        private void BuildNestedDirectory(string dir, List<string> filenames, int size = 4)
+        {
+            if (size <= 0)
+            {
+                return;
+            }
+            Directory.CreateDirectory(dir);
+            var file1 = Path.Combine(dir, "image_1"+size+".jpg");
+            var file2 = Path.Combine(dir, "image_2"+size+".jpg");
+            filenames.Add(file1);
+            filenames.Add(file2);
+
+            File.Create(file1).Dispose();
+            File.Create(file2).Dispose();
+            File.Create(Path.Combine(dir, "banana.jpg")).Dispose();
+            
+            var newdir1 = Path.Combine(dir,"fruits");
+            var newdir2 = Path.Combine(dir,"vegetables");
+            BuildNestedDirectory(newdir1, filenames, size - 1);
+            BuildNestedDirectory(newdir2, filenames, size - 1);
+        }
+
+        private void RemoveDirectory(string path)
+        {
             if (!Directory.Exists(path))
             {
                 return;
@@ -61,6 +98,10 @@ namespace UnitTest
             foreach(string file in files)
             {
                 File.Delete( file );
+            }
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                RemoveDirectory(dir);
             }
             Directory.Delete(path);
         }
